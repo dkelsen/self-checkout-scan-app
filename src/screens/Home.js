bc6ff1ed90b5f4065d7b260cfa899222c37086f9
Dilshan Kelsen <dilshan.kelsen@gmail.com>
2022-04-23 19:24:07 +0200

    Add basic component layout to home screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,31 @@
+import styled from "styled-components";
+
+import Header from "../components/Header";
+import Item, { NewItem } from "../components/Item";
+import Button from "../components/Button";
+
+const CheckoutButton = styled(Button)`
+  margin: auto auto 0 auto;
+`;
+
+const Container = styled.div`
+  height: 100%;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Container>
+      <Header />
+      <h2>Cart</h2>
+      <Item>Item Name</Item>
+      <NewItem />
+      <CheckoutButton>Check Out</CheckoutButton>
+    </Container>
+  );
 };
 
 export default Home;
